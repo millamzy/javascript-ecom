fab53d414a8785aa73977b2cf8dee75fc94d76e6
millamzy <ndefoago@gmail.com>
2024-01-19 16:19:47 +0100

    add 2

--- ec.js
+++ ec.js
@@ -16,7 +16,7 @@ const products = [
     });
   }
   
-  // Function to add product to cart
+// Function to add product to cart
 function addProductToCart(productId) {
     const productToAdd = products.find(product => product.id === productId);
     if (productToAdd) {
@@ -27,7 +27,19 @@ function addProductToCart(productId) {
     }
   }
   
+  // Function to calculate total price and check out
+  function checkOut() {
+    let totalPrice = 0;
+    cart.forEach(item => {
+      totalPrice += item.price;
+    });
+    console.log(`Total price: $${totalPrice}`);
+    cart = [];
+    console.log('Thank you for shopping with us!');
+  }
+  
   // Example usage
   displayProducts();
   addProductToCart(2);
   console.log('Cart:', cart);
+  checkOut();
