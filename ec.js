4d32475082d3ac8d8e550bb6cc1963652f14ad19
Agozilim Ndefo <107751375+millamzy@users.noreply.github.com>
2024-01-21 21:11:55 +0100

    Update ec.js

--- ec.js
+++ ec.js
@@ -1,22 +1,22 @@
 // Define products
 const products = [
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 30 },
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
   ];
   
   // Define cart
   let cart = [];
   
-  // Function to display products
+  // display products
   function displayProducts() {
-    console.log('Available products:');
+    console.log('Products Available:');
     products.forEach(product => {
       console.log(`${product.id}. ${product.name} - $${product.price}`);
     });
   }
   
-// Function to add product to cart
+// add to cart
 function addProductToCart(productId) {
     const productToAdd = products.find(product => product.id === productId);
     if (productToAdd) {
